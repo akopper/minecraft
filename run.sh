975e54491edd7903568060beee7cd39aa1c069ad
Alexander Kopper <alexander.kopper@bitmovin.com>
2019-07-20 18:07:20 +0200

    fix mount

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source config.sh
-docker run -p 25565:25565 --name mc_server --volume ./data:/data:rw  akopper/minecraft:${MINECRAFT_VERSION}
+docker run -p 25565:25565 --name mc_server --volume $(pwd)/data:/data:rw  akopper/minecraft:${MINECRAFT_VERSION}
