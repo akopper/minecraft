d79251b353d6cb77e600f4686d9bbba7351f2b17
Alexander Kopper <alexander.kopper@bitmovin.com>
2019-07-20 14:23:32 +0200

    formatting

--- build_run.sh
+++ build_run.sh
@@ -5,3 +5,4 @@ MC_VERSION=1.14.3
 
 docker build --build-arg MINECRAFT_VERSION=${MC_VERSION} -t akopper/minecraft:${MC_VERSION} .
 docker run -p 25565:25565 --name mc_server akopper/minecraft:${MC_VERSION}
+
