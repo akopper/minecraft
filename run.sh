c1af45deddf7f80374b79b83008731dc976a7753
Alexander Kopper <alexander.kopper@bitmovin.com>
2019-07-20 18:36:45 +0200

    docker run interactive, with terminal and detached

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source config.sh
-docker run -p 25565:25565 --name mc_server --volume $(pwd)/data:/data:rw  akopper/minecraft:${MINECRAFT_VERSION}
+docker run -it -d -p 25565:25565 --name mc_server --volume $(pwd)/data:/data:rw  akopper/minecraft:${MINECRAFT_VERSION}
