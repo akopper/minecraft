62eb37f2c2cb950a230314d5992e201a445703bc
Alexander Kopper <axer@ilogs.com>
2021-01-04 19:12:46 +0100

    rename image name

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 MINECRAFT_VERSION=1.16.4
 MINECRAFT_MAX_MEMORY=2G
 
-docker build --no-cache --build-arg MINECRAFT_VERSION=${MINECRAFT_VERSION} -t akopper/minecraft:${MINECRAFT_VERSION} .
-docker tag akopper/minecraft:${MINECRAFT_VERSION} akopper/minecraft:latest
+docker build --no-cache --build-arg MINECRAFT_VERSION=${MINECRAFT_VERSION} -t akopper/minecraft_server:${MINECRAFT_VERSION} .
+docker tag akopper/minecraft:${MINECRAFT_VERSION} akopper/minecraft_server:latest
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it -d -p 25565:25565 --name mc_server --volume $(pwd)/data:/data:rw  akopper/minecraft:latest
+docker run -it -d -p 25565:25565 --name mc_server --volume $(pwd)/data:/data:rw  akopper/minecraft_server:latest
 docker logs -f mc_server
