059f7368c3a377d699fd5c9d3164600973f92e82
Alexander Kopper <axer@ilogs.com>
2021-01-04 18:46:37 +0100

    no need for env var in build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 MINECRAFT_VERSION=1.16.4
 MINECRAFT_MAX_MEMORY=2G
 
-docker build --no-cache --build-arg MINECRAFT_VERSION=${MINECRAFT_VERSION} --build-arg MINECRAFT_MAX_MEMORY=${MINECRAFT_MAX_MEMORY} -t akopper/minecraft:${MINECRAFT_VERSION} .
+docker build --no-cache --build-arg MINECRAFT_VERSION=${MINECRAFT_VERSION} -t akopper/minecraft:${MINECRAFT_VERSION} .
 docker tag akopper/minecraft:${MINECRAFT_VERSION} akopper/minecraft:latest
